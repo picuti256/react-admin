a3111f8460911bc985405505a4d149fda12cbe55
picuti256 <picuti256@gmail.com>
2023-03-06 15:30:39 -0300

    Ajuste compoente topbar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,3 +1,9 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import { LightModeOutlined } from "@mui/icons-material";
+
 const Topbar = () => {
 	return <div>Topbar</div>;
 };
